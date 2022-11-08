a4c42ceddd026a2937d84d9f14fc497db91c0769
Eduardo Raffi <eduardo.raffi@viavarejo.com.br>
2022-11-08 14:51:34 -0300

    initial commit

---
+++ eraffi-pokedex/pokedex/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by Eduardo Raffi on 08/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func loadView() {
+        view.backgroundColor = .white
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
